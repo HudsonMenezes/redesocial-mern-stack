58efc22d172b3e568e71dbd5d1114e6937d21004
Hudson Menezes <hudson.bm23@gmail.com>
2023-02-15 17:52:03 -0300

    profile card done

--- client/src/components/ProfileCard/ProfileCard.jsx
+++ client/src/components/ProfileCard/ProfileCard.jsx
@@ -10,6 +10,27 @@ const ProfileCard = () => {
         <img src={Cover} alt="cover" />
         <img src={Profile} alt="profile" />
       </div>
+      <div className="ProfileName">
+        <span>Zendaya MJ</span>
+        <span>Senior UI/UX Designer</span>
+      </div>
+
+      <div className="followStatus">
+        <hr />
+        <div>
+          <div className="follow">
+            <span>4.297</span>
+            <span>Following</span>
+          </div>
+          <div className="vl"></div>
+          <div className="follow">
+            <span>1.627</span>
+            <span>Followers</span>
+          </div>
+        </div>
+        <hr />
+      </div>
+      <span>Visualizar Perfil</span>
     </div>
   );
 };
--- client/src/components/profileSide/ProfileSide.jsx
+++ client/src/components/profileSide/ProfileSide.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-
 import LogoSearch from "../LogoSearch/LogoSearch";
-
 import "./ProfileSide.css";
 import ProfileCard from "../ProfileCard/ProfileCard";
+
 const ProfileSide = () => {
   return (
     <div className="ProfileSide">
